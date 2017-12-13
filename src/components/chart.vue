cfcb43258124c3af2e291aa0092e91971436f2ae
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2017-12-13 14:58:02 +0200

    add test chart comp

---
+++ src/components/chart.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="chart-container">
+        {{test}}
+    </div> 
+</template>
+<script>
+    export default {
+        name:'chart',
+        data()=>{
+            test:"test"
+        }
+    }
+</script>
+<style lang="scss">
+    
+</style>
