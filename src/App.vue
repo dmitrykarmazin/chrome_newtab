d28d53ebd9bc169eaf09aba5c559f833ec1b47be
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2017-12-18 14:57:49 +0200

    add loading component

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,56 @@
 <template>
-  <div class="image-container">
-      <lazy-image></lazy-image>
-      
+  <div class="container">
+      <loading-spinner v-if="!isLoaded"></loading-spinner> 
+      <background-layout v-if="isLoaded"></background-layout>
   </div>
 </template>
 <script>
     import { mapState, mapActions } from 'vuex'
-    import lazyImage from './components/lazy-image.vue'
-    import weather from './components/weather.vue'
 
-    
+    import backgroundLayout from './components/background-layout.vue'
+    import loadingSpinner from './components/loading-spinner.vue'
     export default {
         name:'app',
         data(){
             return{
-                name: 'Bob'
+                isLoaded: false
             }
         },
         components:{
-            lazyImage,
-            weather
+            backgroundLayout,
+            loadingSpinner
         },
-        computed:mapState(['currentLocation'])
-        ,
-        methods:mapActions(['GET_DATA'])
-        ,
-        mounted(){
-            //this.$store.dispatch('GET_DATA',this.currentLocation).then(data=>{
-            //    console.log(data)
-            ///})
-            console.log(this.currentLocation)
-            this.GET_DATA(this.currentLocation).then(data=>console.log(data))
-        },
-        beforeCreate() {
-            //this.GET_DATA(this.currentLocation).then(data=>console.log(data))
+        computed:mapState(['currentLocation']),
+        methods:mapActions(['GET_DATA']),
+        created(){
+            this.GET_DATA(this.currentLocation)
+                        .then(()=> {
+                                setTimeout(()=>this.isLoaded = true,500)
+                            })
         }
     }
-
 </script>
 <style lang="scss">
-    @import './assets/_mq.scss';
-    body{
-        background-color: #423030;
-    }
+    @import './assets/_mq.scss';   
     *{
         padding: 0;
         margin: 0;
     }
-    .image-container{
+    
+    .container{
         display:flex;
-        background-color: #1c1c1c;
         width: 100vw;
+        min-height: 560px;
         height: 100vh;
-        @include mobile-only(){
-             background: #355C7D; 
-        }
-        @include desktop(){
-             background: #355C7D;  /* fallback for old browsers */
-             background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);  /* Chrome 10-25, Safari 5.1-6 */
-             background: linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);
-        }
-        .overlay-image{
-            width: 100%;
-        }
-
-    }
-
-    p{
-        color:white
+        justify-content: center;
+        align-items: center;
+        background-color: #EFF0F1;
+        ///@include mobile-only(){
+        //}
+       // @include desktop(){
+             //background: #355C7D;  /* fallback for old browsers */
+             //background: -webkit-linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);  /* Chrome 10-25, Safari 5.1-6 */
+             //background: linear-gradient(to right, #C06C84, #6C5B7B, #355C7D);
+        //}
     }
 </style>
---
+++ src/components/loading-spinner.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="spinner">
+      <div class="cube"></div>
+      <div class="cube"></div>
+  </div>
+</template>
+<script>
+export default {
+  name:'loadingSpinner'
+}
+</script>
+<style lang="scss" scoped>
+  .spinner{
+    width:55px;
+    height: 55px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 50%;
+    animation: spin .7s ease-in-out infinite ;
+    animation: rotate .8s ease-in-out infinite ;
+  
+    animation-fill-mode: forwards;
+  
+    .cube{
+      background-color:#517;
+      width: 10px;
+      height: 10px;
+    }
+    @keyframes spin {
+      50%{
+        transform: scale(1.5);
+      }
+      100%{
+        transform: scale(1.5);
+      }
+    }
+    @keyframes rotate {
+      100%{
+        transform: rotate(360deg);
+      }
+    }
+  }
+</style>
