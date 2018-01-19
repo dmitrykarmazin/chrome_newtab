7cf4ec3c56b589d1f468f7a92de650842d30585f
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-01-19 23:56:32 +0200

    add permissions options in manifest,ref

--- src/main.js
+++ src/main.js
@@ -8,11 +8,13 @@ import addPage from './shared/components/add-page.vue'
 import editPage from './shared/components/edit-page.vue'
 import tabs from './shared/components/tabs.vue'
 import tab from './shared/components/tab.vue'
+import bookmarkTree from './shared/components/bookmark-tree.vue'
 Vue.use(VueClazyLoad)
 Vue.component(addPage.name, addPage)
 Vue.component(editPage.name, editPage)
 Vue.component(tabs.name, tabs)
 Vue.component(tab.name, tab)
+Vue.component(bookmarkTree.name, bookmarkTree)
 new Vue({
     el:'#app',
     store,
